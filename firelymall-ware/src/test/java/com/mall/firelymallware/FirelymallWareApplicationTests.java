c79e0ea01e39b0d726fac5f4490adb61e1e06669
TheFoxFairy <1479852727@qq.com>
2022-10-03 14:52:40 +0800

    项目初始化

---
+++ firelymall-ware/src/test/java/com/mall/firelymallware/FirelymallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.firelymallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirelymallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
